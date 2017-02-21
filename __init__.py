79611d27d34e13b8d36b4652a266710ea1d0dc41
Heroist Hero <firestinger_1@hotmail.com>
2017-02-21 15:26:11 -0700

    Laid out psuedo code

---
+++ __init__.py
@@ -0,0 +1,17 @@
+
+
+#Connector_list
+#MemShare
+
+def main():
+    #Await Signal from Window
+    
+    #THREAD
+        #REPEAT
+            #Accept connection requests from Slices
+
+    #REPEAT
+        #Check for updated memshare
+        #divide updated memshare and send along connector_list
+        #retrieve work and put back into memshare
+    
